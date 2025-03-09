d44881a2fd71972d0abd20a12097547758ffb90c
Sai Prasad <Sai Prasad@LAPTOP-5R43AL2Q>
2025-03-09 11:08:36 +0530

    function commit

--- src/test/java/commonFunction/FunctionLibrary.java
+++ src/test/java/commonFunction/FunctionLibrary.java
@@ -1,5 +1,120 @@
 package commonFunction;
 
-public class FunctionLibrary {
+import static org.testng.Assert.assertEquals;
 
+import java.io.FileInputStream;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
+
+
+
+public class FunctionLibrary {
+	public static WebDriver driver;
+	public static Properties conpro;
+	//method for launching browser
+	public static WebDriver startBrowser()throws Throwable
+	{
+		conpro = new Properties();
+		conpro.load(new FileInputStream("./PropertyFiles/Environment.Properties"));
+		if (conpro.getProperty("Browser").equalsIgnoreCase("chrome"))
+		{
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+		}
+		else if(conpro.getProperty("Browser").equalsIgnoreCase("firefox"))
+		{
+			driver = new FirefoxDriver();
+			driver.manage().window().maximize();
+		}
+		else
+		{
+			Reporter.log("Browser value is not matching",true);
+		}
+		return driver;
+	}
+	//method for launching url
+	public static void openUrl()
+	{
+		driver.get(conpro.getProperty("url"));
+	}
+//method for wait an element
+	public static void waitForElement(String LocatorType,String LocaterValue,String TestData)
+	{
+		WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(Integer.parseInt(TestData)));
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			//wait until element is visible
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(TestData)));
+		}
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			//wait until element is visible
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.name(TestData)));
+		}
+		if(LocatorType.equalsIgnoreCase("id"))
+		{
+			//wait until element is visible
+			mywait.until(ExpectedConditions.visibilityOfElementLocated(By.id(TestData)));
+		}
+	}
+	public static void typeAction(String LocatorType,String LocaterValue,String TestData)
+	{
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(TestData)).clear();
+			driver.findElement(By.xpath(TestData)).sendKeys(TestData);
+		}
+		if (LocatorType.equals("name")) 
+		{
+			driver.findElement(By.name(TestData)).clear();
+			driver.findElement(By.name(TestData)).sendKeys(TestData);
+		}
+		if (LocatorType.equals("id")) 
+		{
+			driver.findElement(By.id(TestData)).clear();
+			driver.findElement(By.id(TestData)).sendKeys(TestData);
+		}
+	}
+	//method for button,checkbox,radio button,images,links
+	public static void clickAction(String LocatorType,String LocaterValue)
+	{
+		if(LocatorType.equalsIgnoreCase("xpath"))
+		{
+			driver.findElement(By.xpath(LocaterValue)).click();
+		}
+		if(LocatorType.equalsIgnoreCase("name"))
+		{
+			driver.findElement(By.name(LocaterValue)).click();
+		}
+		if (LocatorType.equalsIgnoreCase("id"))
+		{
+			driver.findElement(By.id(LocaterValue)).sendKeys(Keys.ENTER);
+			
+		}
+
+	}
+	//method for validate page title
+	public static void validateTitle(String Expected_Title)
+	{
+		String Actual_title = driver.getTitle();
+		try {
+			assertEquals(Actual_title, Expected_Title,"Title is not Matching");
+		} catch (AssertionError a) {
+			System.out.println(a.getMessage());
+		}
+	}
+	//method for close browser
+	public static void closeBrowser()
+	{
+		driver.quit();
+	}
 }
